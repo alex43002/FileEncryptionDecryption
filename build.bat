c44ec902eadfb618251d79290840f959188b7b95
Alexander DeJesus <alexdejesus336@Yahoo.com>
2024-10-01 18:54:42 -0400

    Build cpp files in one call

---
+++ build.bat
@@ -0,0 +1,25 @@
+@echo off
+
+rem Get the directory of the batch file
+set "SCRIPT_DIR=%~dp0"
+
+rem Change to the script directory
+cd /d "%SCRIPT_DIR%"
+
+rem Set the output executable name
+set OUTPUT=encryption_tool.exe
+
+rem Delete the previous build if it exists
+if exist %OUTPUT% (
+    del %OUTPUT%
+)
+
+rem Compile all .cpp files in the directory
+g++ *.cpp -o %OUTPUT%
+
+rem Check if the compilation was successful
+if %errorlevel% neq 0 (
+    echo Build failed.
+) else (
+    echo Build successful! Run with %OUTPUT% [encrypt/decrypt] [input_file] [output_file]
+)
