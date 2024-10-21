c1c030c63cb315a4ccf9e3040170265cc58c5e9e
Alexander DeJesus <alexdejesus336@Yahoo.com>
2024-10-20 21:50:09 -0400

    Updated help file

--- util/help/Help.cpp
+++ util/help/Help.cpp
@@ -2,10 +2,17 @@
 #include <iostream>
 
 void displayHelp() {
-    std::cout << "Usage: encryption_tool [options] [arguments]\n";
+    std::cout << "Usage: encryption_tool.exe [options] <encryption_type> <encryption_key> <input_file> <output_file>\n";
     std::cout << "\nOptions:\n";
-    std::cout << "  --help                Show this help message and exit.\n";
-    std::cout << "  --encrypt <file>     Encrypt the specified file.\n";
-    std::cout << "  --decrypt <file>     Decrypt the specified file.\n";
-    std::cout << "\nThis tool provides encryption and decryption functionalities.\n";
+    std::cout << "  --help                            Show this help message and exit.\n";
+    std::cout << "  --encrypt                         Encrypt the specified input file.\n";
+    std::cout << "  --decrypt                         Decrypt the specified input file.\n";
+    std::cout << "\nArguments:\n";
+    std::cout << "  <encryption_type>                 The encryption algorithm to use (e.g., DES).\n";
+    std::cout << "  <encryption_key>                  The key used for encryption or decryption.\n";
+    std::cout << "  <input_file>                      The file to encrypt or decrypt.\n";
+    std::cout << "  <output_file>                     The output file where the result will be saved.\n";
+    std::cout << "\nExamples:\n";
+    std::cout << "  encryption_tool.exe --encrypt DES my_secret_key input.txt encrypted_output.txt\n";
+    std::cout << "  encryption_tool.exe --decrypt DES my_secret_key encrypted_output.txt decrypted_output.txt\n";
 }
